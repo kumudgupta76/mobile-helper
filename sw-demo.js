0c5692bdf630ebce1a87cdfd126f279f0691cd07
Kumud Gupta <40192898+kumudgupta76@users.noreply.github.com>
2024-06-06 12:07:37 +0000

    Updates

--- sw-demo.js
+++ sw-demo.js
@@ -9,32 +9,55 @@ const urlsToCache = [
   // Add other assets you want to cache
 ];
 
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(error => console.error('Failed to open cache:', error))
+  );
+});
 
-self.addEventListener("install", event =>
-event.waitUntil(
-  caches.open(CACHE_NAME)
-    .then(cache => cache.addAll(urlsToCache))
-);
-);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request).then(fetchResponse => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        });
+      }).catch(() => {
+        return caches.match('/index.html');
+      })
+  );
+});
 
-self.addEventListener("fetch", event => 
-event.respondWith(
-  caches.match(event.request)
-    .then(response => response || fetch(event.request))
-);
-);
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 
-self.addEventListener("push", payload => {
-    console.log("[Service Worker] Push Received.", payload.data.json());
-    const notificationTitle = 'Notification !!!'
-    const notificationOptions = {
-      body: 'This is just a test notification sent from local servet',
-      data: 'you can provide additional data here',
-      icon: 'battery.png'
-    };
-  
-    return self.registration.showNotification(
-      notificationTitle,
-      notificationOptions
-    );
-  });
+self.addEventListener("push", event => {
+  console.log("[Service Worker] Push Received.", event.data.json());
+  const notificationTitle = 'Notification !!!';
+  const notificationOptions = {
+    body: 'This is just a test notification sent from local server',
+    data: 'you can provide additional data here',
+    icon: 'battery.png'
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(notificationTitle, notificationOptions)
+  );
+});
